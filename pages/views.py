1f62ad9eff89df2c25161b3b21f6ebee979164dc
T.Czosnyka <t.czosnyka90@gmail.com>
2023-04-26 14:40:10 +0200

    products dispalying in table in home view added

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.apps import apps
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home_view(request):
     Product = apps.get_model('products', 'Product')
     products = Product.objects.all()
-    context = {'products': products}
+    paginator = Paginator(products, 3)
+
+    context = {'products': products, 'paginator': paginator}
     return render(request, 'pages/home.html', context)
 
 def about_view(request):
