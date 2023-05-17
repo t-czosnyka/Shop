5f12ee0984ac3f23c2efa00032425f685e2f8ba1
T.Czosnyka <t.czosnyka90@gmail.com>
2023-05-17 12:24:32 +0200

    order without login added

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'orders'
 urlpatterns = [
     path('data/', views.order_data_view, name='data'),
+    path('nologin/', views.no_login_order, name='nologin'),
     path('create/', views.order_create_view, name='create')
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,28 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from products.cart import get_cart_products_specific_all, clear_cart
 from .models import Order, OrderProducts
 from django.contrib import messages
 from .forms import OrderForm
+from django.conf import settings
 # Create your views here.
 
 
 def order_data_view(request):
-    if request.method == "GET":
-        if not request.user.is_authenticated:
-            redirect()
-        form = OrderForm()
-        context = {
-            'title': 'Order data',
-            'form': form
-        }
-        return render(request, 'orders/order_data.html', context)
-
+    if not request.user.is_authenticated and not request.session.get('no_login_order', False):
+        url = settings.LOGIN_URL + '?next=' + request.path + '&order=True'
+        return redirect(url)
+    products = get_cart_products_specific_all(request)
+    if len(products) == 0:
+        messages.warning(request, "No products to order.")
+        return redirect('pages:home')
+    form = OrderForm(request.POST or None)
     if request.method == "POST":
-        products = get_cart_products_specific_all(request)
-        if len(products) == 0:
-            messages.warning(request, "No products to order.")
-            return redirect('pages:home')
-        form = OrderForm(request.POST)
         if form.is_valid():
             order_object = form.save()
             # Create order products, not using bulk_create to call save method
@@ -32,6 +26,11 @@ def order_data_view(request):
             clear_cart(request)
             messages.success(request, "Your order has been created.")
         return redirect('pages:home')
+    context = {
+        'title': 'Order data',
+        'form': form
+    }
+    return render(request, 'orders/order_data.html', context)
 
 
 def order_create_view(request):
@@ -43,6 +42,11 @@ def order_create_view(request):
     o.save()
     # Create order products, not using bulk_create to call save method
     for product in products:
-        op = OrderProducts(order = o, product_specific=product)
+        op = OrderProducts(order=o, product_specific=product)
         op.save()
     return redirect('pages:home')
+
+
+def no_login_order(request):
+    request.session['no_login_order'] = True
+    return redirect('orders:data')
