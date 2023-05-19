a3799e72022d37a14dcc11ffe8b5b1b2adf33a0f
T.Czosnyka <t.czosnyka90@gmail.com>
2023-05-19 16:10:54 +0200

    paginator on home page deleted

--- pages/views.py
+++ pages/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 from django.apps import apps
-from django.core.paginator import Paginator
 # Create your views here.
+
 
 def home_view(request):
     Product = apps.get_model('products', 'Product')
     products = Product.objects.all()
     for product in products:
         product.assign_main_img()
-    paginator = Paginator(products, 3)
-
-    context = {'products': products, 'paginator': paginator}
+    context = {'products': products}
     return render(request, 'pages/home.html', context)
+
 
 def about_view(request):
     context = {}
     return render(request, 'pages/about.html', context)
+
 
 def contact_view(request):
     context = {}
