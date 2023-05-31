17ae7d1b0d1345dc07373ffad9dfafe434abeec4
T.Czosnyka <t.czosnyka90@gmail.com>
2023-05-31 12:01:37 +0200

    Filtering foreign key fields in admin added

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
-from .models import Product, ProductShoe, Producer, Color, ProductSuit, ProductShirt, ProductImage, ProductMainImage, Rating
+from .models import Product, ProductShoe, Producer, Color, ProductSuit, ProductShirt,\
+    ProductImage, ProductMainImage, Rating, ProductSpecific
+from django.forms import ModelForm
+from django.db.models import ObjectDoesNotExist
 
 # Register your models here.
-
-
 admin.site.register(Producer)
-admin.site.register(ProductShoe)
-admin.site.register(ProductSuit)
-admin.site.register(ProductShirt)
-admin.site.register(ProductMainImage)
+
 admin.site.register(Color)
 admin.site.register(Rating)
 
@@ -21,5 +19,31 @@ class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ('avg_rating',)
 
 
+class ProductSpecificAdmin(admin.ModelAdmin):
+    # Filter products only matching ProductSpecific TYPE.
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "product":
+            kwargs["queryset"] = Product.objects.filter(type=self.model.TYPE)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+# Filter ProductImages only referring to set Product.
+class MainImageForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            self.fields['main_img'].queryset = ProductImage.objects.filter(product=self.instance.product)
+        except ObjectDoesNotExist:
+            pass
+
+
+class ProductMainImageAdmin(admin.ModelAdmin):
+
+    form = MainImageForm
+
+
 admin.site.register(ProductImage, ProductImageAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductShoe, ProductSpecificAdmin)
+admin.site.register(ProductSuit, ProductSpecificAdmin)
+admin.site.register(ProductShirt, ProductSpecificAdmin)
+admin.site.register(ProductMainImage, ProductMainImageAdmin)
