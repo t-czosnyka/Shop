c618ef466b3ba3806604bc4cde54d4762917a2b4
T.Czosnyka <t.czosnyka90@gmail.com>
2023-06-05 11:38:25 +0200

    id changed to pk

--- orders/urls.py
+++ orders/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'orders'
 urlpatterns = [
     path('data/', views.order_data_view, name='data'),
-    path('<int:id>/', views.order_detail_view, name='detail'),
+    path('<int:pk>/', views.order_detail_view, name='detail'),
     path('confirm/<slug:oidb64>/<slug:token>', views.order_confirm_view, name='confirm'),
     path('checkout/success', views.checkout_successful_view, name='checkout_success'),
     path('checkout/cancelled', views.checkout_cancelled_view, name='checkout_cancelled'),
