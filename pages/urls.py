59675d8256366fd4d5c8da3b765f25be204555be
T.Czosnyka <t.czosnyka90@gmail.com>
2023-06-14 13:43:57 +0200

    about and contact pages deleted

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,4 @@ from . import views
 app_name = 'pages'
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('about', views.about_view, name='about'),
-    path('contact', views.contact_view, name='contact'),
-
 ]
