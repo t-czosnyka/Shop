39c888af287317ded5e4b9faed3f4997d060ee76
T.Czosnyka <t.czosnyka90@gmail.com>
2023-06-30 17:24:55 +0200

    ProductList, ProductDetail and ProductSpecifiDetail urls created

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'api'
+
 urlpatterns = [
-    path('products/', views.ProductView.as_view(), name="products"),
-    path('products/<int:product_pk>', views.ProductView.as_view(), name="products"),
-    path('products/<int:product_pk>/<int:product_specific_pk>', views.ProductSpecificView.as_view(), name="products"),
-    path('products/create/', views.ProductCreateView.as_view(), name="product-create"),
+    path('products/', views.ProductListView.as_view(), name="product-list"),
+    path('products/<int:product_pk>', views.ProductDetailView.as_view(), name="product-detail"),
+    path('products/<int:product_pk>/<int:product_specific_pk>', views.ProductSpecificDetailView.as_view(),
+         name="product-specific-detail"),
 ]
 
